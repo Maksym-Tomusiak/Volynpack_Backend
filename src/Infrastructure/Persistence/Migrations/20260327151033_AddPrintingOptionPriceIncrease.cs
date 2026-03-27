5a7c95ee7b611ea055982f2f8556ee2ecac7c145
Maksym-Tomusiak <maxtomusiak315@gmail.com>
2026-03-27 19:08:31 +0200

    feat: add priceIncrease migration

---
+++ src/Infrastructure/Persistence/Migrations/20260327151033_AddPrintingOptionPriceIncrease.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Infrastructure.Persistence.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddPrintingOptionPriceIncrease : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<float>(
+                name: "price_increase",
+                table: "PrintingOptions",
+                type: "real",
+                nullable: false,
+                defaultValue: 0f);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "price_increase",
+                table: "PrintingOptions");
+        }
+    }
+}
